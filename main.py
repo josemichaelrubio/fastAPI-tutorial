967a52be55314811d9cf571a99b48c81f155104c
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-09-13 16:20:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
